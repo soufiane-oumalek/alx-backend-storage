9300e7886082a7f7d823aef58516653a4d17a8c5
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-01-17 21:37:50 +0100

    mysql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,7 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
