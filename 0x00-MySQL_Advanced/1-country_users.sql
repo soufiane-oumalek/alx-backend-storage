8b39be3a375e419c981bded2c7eebd5e69bc157d
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-01-18 21:49:54 +0100

    mysql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,8 @@
 -- creates a table for users
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
     country ENUM('US', 'CO', 'TN') NOT NULL,
+    PRIMARY KEY (id)
 );
